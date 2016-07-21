40b5ffa49aa5901db11bc23947ac2807e2519412
davidedelerma <davide.delerma@gmail.com>
2016-07-21 09:49:25 +0100

    maps

--- public/index.js
+++ public/index.js
@@ -1,6 +1,77 @@
+var Map = function( latLng, zoom ){
+
+  this.googleMap = new google.maps.Map(document.getElementById( 'map' ),{
+      center: latLng,
+      zoom: zoom
+  })
+
+  this.addMarker = function(latLng,title) {
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: this.googleMap,
+      title: title
+    })
+    return marker
+  }
+
+  this.addInfoWindow = function(latLng,title){
+    var marker=this.addMarker( latLng,title )
+    marker.addListener('click',function(){
+       var infoWindow = new google.maps.InfoWindow({
+        content: this.title
+       })
+       infoWindow.open(this.map, marker)
+    })
+  }
+
+}
+
+var GeoLocator = function(){
+  //this.map = map;
+
+  this.getCoord = function(){
+    navigator.geolocation.getCurrentPosition(function(position){
+      pos = {lat: position.coords.latitude, lng: position.coords.longitude};
+      getMyCountry(pos);
+      var map = new Map(pos,5);
+      var marker = new google.maps.Marker({
+        position: pos,
+        map: map.googleMap
+      })
+    }.bind(this))
+  }
+}
 
 var state = {
   countries: []
+}
+
+function getMyCountry(pos){
+
+     //extract long and lat
+     var lat = pos.lat;
+     var lng = pos.lng;
+     //concatanate corrent url with long and lat 
+     url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&sensor=true'
+     //start a new request
+     var request = new XMLHttpRequest();
+     request.open("GET", url);
+     request.onload = function(){
+       if(request.status === 200) {
+         var jsonString = request.responseText;
+         var locationObject = JSON.parse(jsonString)
+         //create function to filter through address_components to be mroe dynamic...
+         var currentCountry = locationObject.results[0].address_components[5].long_name;
+         console.log("COUNTRY: ", currentCountry)
+         var currCountryBox = document.getElementById('current-country');
+         currCountryBox.innerHTML= " "
+         var p = document.createElement( 'p' );
+         p.innerHTML = "You are in: "+currentCountry;
+         currCountryBox.appendChild(p);
+       }
+     }
+     //send request
+     request.send( null );
 }
 
 window.onload = function(){
@@ -8,6 +79,7 @@ window.onload = function(){
 }
 
 function takeCountries(){
+
   var url = 'https://restcountries.eu/rest/v1';
   var request = new XMLHttpRequest();
   request.open("GET", url);
@@ -35,6 +107,7 @@ function main(){
 //   countryList.appendChild(dropdown);
 
 //CONTESTUAL DROPDOWN
+
   regionList(state.countries)
   var dropdown = document.createElement('select');
   for (region of regions){
@@ -54,7 +127,7 @@ function main(){
   countryList.appendChild(dropdown);
 
 // take selected item and show + show border countries
-  dropdown.onchange= function(){
+  dropdown.onchange = function(){
     //find selected item in contestual dropdown
     selectedIndex = dropdown.selectedIndex;
     selectedElement = dropdown.options[selectedIndex].value;
@@ -70,11 +143,18 @@ function main(){
     //to find item in dropdown menu:
     //selectedItem = dropdown.selectedIndex;
     //
-    showSelected(newIndex);
+    var center = showSelected(newIndex);
+    var map = new Map(center,5);
+    string = '<h3>'+state.countries[newIndex].name+'</h3>'+'<p>'+ 'population: '+state.countries[newIndex].population+'</p>'+'<p>'+ 'Capital City:' +state.countries[newIndex].capital+'</p>'
+    map.addInfoWindow(center, string);
     persitCountries(newIndex);
     borderingCountries = borderCountries(newIndex);
     displayBorders(borderingCountries);
   }
+
+  var geo = new GeoLocator();
+  var button = document.getElementById('get-my-location');
+  button.onclick = geo.getCoord;
 }
 
 
@@ -83,11 +163,13 @@ var showSelected = function(countryIndex) {
   var displaybox = document.getElementById('display-box');
   displaybox.innerHTML= " "
   var country = state.countries[countryIndex]
+  var center = {lat: country.latlng[0], lng: country.latlng[1]}
   var div = document.createElement( 'div' );
   var p = document.createElement( 'p' );
   p.innerHTML = country.name + ": Population: " + country.population + ", Capital City: " + country.capital;
   div.appendChild(p);
   displaybox.appendChild(div);
+  return center;
 }
 
 var persitCountries = function(countryIndex){
